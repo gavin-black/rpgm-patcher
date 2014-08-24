5c0eb3cc388a9f1ad6853ab052116fe9a65d459c
U-media\gblack <gblack@media.(none)>
2014-08-23 20:44:58 -0400

    Small code cleanup

--- patchCreator.rb
+++ patchCreator.rb
@@ -2,7 +2,33 @@ def readInt()
   $s.read(4).unpack('l')[0]
 end
 
-puts "Start"
+def unpackV3
+  key = readInt()*9 + 3
+  keyBytes = [key].pack("l").unpack('C*')
+  puts "Key: #{key.to_s(16)} -- #{keyBytes}"
+
+  loop do
+    offset = key ^ readInt()
+    break if offset <= 0
+
+    size = key ^ readInt()
+    fkey = key ^ readInt()
+    length = key ^ readInt()
+
+    puts "File: -- Offset: #{offset} -- Size: #{size} --" +
+         " Key: #{fkey.to_s(16)} -- Length: #{length}"
+
+    nameEnc = $s.read(length)
+
+    nameDec = ""
+
+    for i in 0..(length - 1)
+      nameDec += (nameEnc[i].unpack('C')[0] ^ keyBytes[i%4]).chr
+    end
+
+    puts "File name: #{nameDec}"
+  end 
+end
 
 version = 0
 $s = File.new('Game.rgss3a', 'rb')
@@ -17,27 +43,4 @@ if version == 0
 end
 puts "Version is: #{version[0]}"
 
-key = readInt()
-key *= 9
-key += 3
-keyBytes = [key].pack("l").unpack('C*')
-puts "Key: #{key.to_s(16)} -- #{keyBytes}"
-
-offset = key ^ readInt()
-size = key ^ readInt()
-fkey = key ^ readInt()
-length = key ^ readInt()
-
-puts "File: -- Offset: #{offset} -- Size: #{size} --" +
-     " Key: #{fkey.to_s(16)} -- Length: #{length}"
-
-nameEnc = $s.read(length)
-
-nameDec = ""
-
-for i in 0..(length - 1)
-  nameDec += (nameEnc[i].unpack('C')[0] ^ keyBytes[i%4]).chr
-end
-
-puts "File name: #{nameDec}"
-
+unpackV3()
